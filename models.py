af309f259453ef5fa4f2f358e81f122f9848dad4
Nikolas-Batista <nikolas.0605@gmail.com>
2025-05-28 21:21:35 -0400

    update

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ db = SQLAlchemy()
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), unique=True, nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
     quantidade_estoque = db.Column(db.Integer, default=0)
 
 class Doacao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
-    doador = db.Column(db.String(100), nullable=False)
+    doador = db.Column(db.String(100), nullable=True)
     data = db.Column(db.DateTime, default=datetime.utcnow)
 
     item = db.relationship('Item')
